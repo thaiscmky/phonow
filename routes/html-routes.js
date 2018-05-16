4724d6999f542c422dccdb6fcc2706feb1302082
The Mai <mai.the.09@gmail.com>
2018-05-15 19:56:49 -0500

    changed html-routes to reflect to hdbrs structure

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,22 +4,22 @@ const router = express.Router();
 // -------- Homepage route
 router.get('', (req,res) => {
     const title='Welcome to Pho Now!';
-    res.render('index', {title: title });
+    res.render('./main/index', {title: title });
 });
 
 // --------------- Menu
 router.get('/menu', (req, res) => {
-    res.render('menu');
+    res.render('./main/menu');
 });
 
 // --------------- Store Info
 router.get('/about', (req, res) => {
-    res.render('about');
+    res.render('./main/about');
 });
 
 // --------------- Contact Us
 router.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('./main/contact');
 });
 
 module.exports = router;
