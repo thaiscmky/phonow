72682a87691f1d4cad0fe245df847e08898b5263
Yewbdar <Yewbdar.grima@gmail.com>
2018-05-16 16:02:36 -0500

    #101 table CREATE teast done

--- test/test.js
+++ test/test.js
@@ -134,9 +134,111 @@ describe('Load a Page', function () {
                 .catch(done);
         });
     });
-   
+//create test
+var restaurant={restaurant_name:"phonow"};
+ 
+// creatTable(db.restaurant,restaurant);
+// function creatTable( table,data){
+
+    describe('/ (SEQUELIZE : CRUD - CREATE Restaurant)', () => {
+        it('It should create Restaurant table', function () {
+            return db.sequelize.sync({ force: true }).then(() => {
+                return db.restaurant.create({
+                    restaurant_name:"phonow"
+                }).then(() => { console.log('data created') }).
+                    catch(err => { console.log(err) });
+                //   done();
+            })
+        })
+    });
+// }
 
 
+    describe('/ (SEQUELIZE : CRUD - CREATE userSecGrps)', () => {
+        it('It should create userSecGrps table', function () {
+            return db.sequelize.sync().then(() => {
+              return db.User_sec_grp.create({ 
+                grp_code: "admin",grp_name:"administrator",
+                });
+              done();
+            })    
+          })
+        }); 
+    
+
+    describe('/ (SEQUELIZE : CRUD - CREATE menu_type)', () => {
+        it('It should create menu_type table', function () {
+            return db.sequelize.sync().then(() => {
+                return db.menu_type.create({ MenuType_name: "lunch", MenuType_description: "food for lunch" });
+                done();
+            })
+        })
+    });
+
+    describe('/ (SEQUELIZE : CRUD - CREATE menu_category)', () => {
+        it('It should create menu_category table', function () {
+            return db.sequelize.sync().then(() => {
+                return db.menu_category.create({ category_name: "beef noodle soup", MenuType_description: "so yummy" });
+                done();
+            })
+        })
+    });
+            describe('/ (SEQUELIZE : CRUD - CREATE relation between rsturant and restaurant_hour )', () => {
+                it('It should create relation between rsturant and restaurant_hour  ', function () {
+                    return db.sequelize.sync().then(() => {
+                      return db.restaurant_hour.create({ day_name: "Monday",
+                      start_time:"8:00 am",
+                      end_time:"6:30 pm",
+                      restaurantId:1});
+                      done();
+                    })    
+                  })
+            }); 
+
+            describe('/ (SEQUELIZE : CRUD - CREATE relation between user and UserSecGrp  )', () => {
+                it('It should create relation between user and UserSecGrp  ', function () {
+                    return db.sequelize.sync().then(() => {
+                      return db.user.create({ user_first_name: "Yewbdar",
+                      user_last_name:"Abera",
+                      user_email:"you@me.com",
+                      UserSecGrpGrpCode:"admin"});
+                      done();
+                    })    
+                  })
+            }); 
+
+            describe('/ (SEQUELIZE : CRUD - CREATE relation between menu_items with menu_type and menu_category )', () => {
+                it('It should create relation between menu_items and menu_type  ', function () {
+                    return db.sequelize.sync().then(() => {
+                      return db.menu_items.create({ item_name_english: "food",
+                      item_name_vietnamese:"vihsueksenje",
+                      item_price:"10.00",
+                      menuTypeId:1,  
+                      menuCategoryId:1  
+                      });
+                      done();
+                    })    
+                  })
+            }); 
+
+            describe('/ (SEQUELIZE : CRUD - CREATE relation between restaurant_contact and restaurant )', () => {
+                it('It should create relation between restaurant_contact and restaurant  ', function () {
+                    return db.sequelize.sync().then(() => {
+                      return db.restaurant_contact.create({ social_contact: "facebook/phnow",
+                      contact_email:"phonow@gmail.com",
+                      contact_phone:"123123123",
+                      restaurantId:1
+                      });
+                      done();
+                    })    
+                  })
+            }); 
+
+
+    // update test 
+
+    
 
 });
+
 
