677154d67f5407f20f012c2559be25ec7f7a431a
Vinanti <vinantinaik@gmail.com>
2018-05-17 10:06:33 -0500

    Insert Menu Item

--- models/menuCategory.js
+++ models/menuCategory.js
@@ -1,24 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
-    var menuCategory= sequelize.define('menu_category', {
-        category_name:{
-        type:DataTypes.STRING,
-      },
-      category_description:{
-       type:DataTypes.STRING
-      },
-       isActive:{
-        type:DataTypes.BOOLEAN ,
-        defaultValue:false
-       },
-       created_by:{
-        type:DataTypes.STRING
-       },
-       updated_by:{
-        type:DataTypes.STRING
-       },
-    }, {});
-    menuCategory.associate = function(models) {
-      
-      };
-    return menuCategory;
+  var menuCategory = sequelize.define('menu_category', {
+    category_name: {
+      type: DataTypes.STRING,
+    },
+    category_description: {
+      type: DataTypes.STRING
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    created_by: {
+      type: DataTypes.STRING
+    },
+    updated_by: {
+      type: DataTypes.STRING
+    },
+  }, {});
+  menuCategory.associate = function (models) {
+
   };
+  return menuCategory;
+};
