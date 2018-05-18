12e6eb7a81815b7afb0ca544c569ccbf7c9593f7
The Mai <mai.the.09@gmail.com>
2018-05-17 20:08:33 -0500

    reformat html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,100 +3,103 @@ const router = express.Router();
 const db = require('../models');
 
 // -------- Homepage route
-router.get('', (req,res) => {
-    const title='index';
-    res.render('./main/index', {title: title });
+router.get('', (req, res) => {
+    const title = 'index';
+    res.render('./main/index', { title: title });
 });
 
 // --------------- Menu
 router.get('/menu', (req, res) => {
-    const title='menu';
-    res.render('./main/menu', {title: title });
+    const title = 'menu';
+    res.render('./main/menu', { title: title });
 });
 
 // --------------- Store Info
 router.get('/about', (req, res) => {
-    const title='about';
-    res.render('./main/about', {title: title });
+    const title = 'about';
+    const hours = {
+
+    }
+    res.render('./main/about', { title: title });
 });
 
 // --------------- Contact Us
 router.get('/contact', (req, res) => {
-    const title='contact';
-    res.render('./main/contact', {title: title});
+    const title = 'contact';
+    res.render('./main/contact', { title: title });
 });
 
 
 
 //add user 
-router.post('/user/:id/:fName/:lName/:email/:secGrpId',(req,res)=>{
- db.sequelize.sync().then(() => {
-    db.user.create({
-        user_first_name:req.params.fName,
-        user_last_name:req.params.lName,
-        user_email: req.params.email,
-        UserSecGrpGrpCode: req.params.secGrpId,
-    });
-    done();
-})
+router.post('/user/:id/:fName/:lName/:email/:secGrpId', (req, res) => {
+    db.sequelize.sync().then(() => {
+        db.user.create({
+            user_first_name: req.params.fName,
+            user_last_name: req.params.lName,
+            user_email: req.params.email,
+            UserSecGrpGrpCode: req.params.secGrpId,
+        });
+        done();
+    })
 });
 
 
 // get user
-router.get('/user',(req,res)=>{
+router.get('/user', (req, res) => {
     db.sequelize.sync().then(() => {
-          db.user.findAll({where:{isActive:true}}).then((user) =>{
- res.render('./admin/user', {layout:'main-admin', title: "System user", settings: user});
-   });
-         done();
-     })
- });
+        db.user.findAll({ where: { isActive: true } }).then((user) => {
+            res.render('./admin/user', { layout: 'main-admin', title: "System user", settings: user });
+        });
+        done();
+    })
+});
 // update user 
-router.put('/user/:id/:fName/:lName/:email',(req,res)=>{
+router.put('/user/:id/:fName/:lName/:email', (req, res) => {
     db.sequelize.sync().then(() => {
-          db.user.update({
-             user_first_name:req.params.fName,
-             user_last_name:req.params.lName,
-             user_email: req.params.email,
-         }, { where: { user_id:req.params.id } });
-         done();
-     })
- 
- });
+        db.user.update({
+            user_first_name: req.params.fName,
+            user_last_name: req.params.lName,
+            user_email: req.params.email,
+        }, { where: { user_id: req.params.id } });
+        done();
+    })
 
- // delete user 
- router.put('/user/:id',(req,res)=>{
+});
+
+// delete user 
+router.put('/user/:id', (req, res) => {
     db.sequelize.sync().then(() => {
-          db.user.update({
-             isActive: false
-         }, { where: { user_id:req.params.id } });
-         done();
-     })
- });
- 
+        db.user.update({
+            isActive: false
+        }, { where: { user_id: req.params.id } });
+        done();
+    })
+});
+
 //update categories
-router.put('/categories/:id/:categName/:categDescription',(req,res)=>{
+router.put('/categories/:id/:categName/:categDescription', (req, res) => {
     db.sequelize.sync().then(() => {
-          db.user.update({
-            category_name:req.params.categName,
-            category_description:req.params.categDescription,
-            
-         }, { where: { id:req.params.id } });
-         done();
-     })
- 
- });
+        db.user.update({
+            category_name: req.params.categName,
+            category_description: req.params.categDescription,
 
- //delete categories 
+        }, { where: { id: req.params.id } });
+        done();
+    })
 
- router.put('/categories/:id',(req,res)=>{
+});
+
+//delete categories 
+
+router.put('/categories/:id', (req, res) => {
     db.sequelize.sync().then(() => {
-          db.menu_category.update({
-             isActive: false
-         }, { where: { id:req.params.id } });
-         done();
-     })
- });
+        db.menu_category.update({
+            isActive: false
+        }, { where: { id: req.params.id } });
+        done();
+    })
+});
 
- 
+
 module.exports = router;
