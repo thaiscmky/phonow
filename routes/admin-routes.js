3d03047f5e010e10e11409c02b334fcab0d4cd38
Thais Cailet <thais.cailet@gmail.com>
2018-05-18 18:01:29 -0500

    Revert "Merge branch 'menu_item_add' into staging"
    This reverts commit 6b3aae67619c31eebc9879f495d6b62e0b7f2f85, reversing
    changes made to eef935844daabb287db86e237652848bbc42d906.

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../helpers/auth');
+var db = require("../models");
 
-const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
-let db = require("../models");
+
 // -------- Homepage route
-router.get('/', (req, res) => {
-    const title = 'Pho Now Administrator Dashboard';
-    res.render('./admin/index', { layout: 'main-admin', title: title });
-});
-
-// -------- sample dashboard route
-router.get('/dash', (req, res) => {
-    const title = 'Welcome to Pho Now!';
-    res.render('./admin/dash-sample', { layout: 'main-admin', title: title });
+router.get('/', (req,res) => {
+    const title='Pho Now Administrator Dashboard';
+    res.render('./admin/index', {layout:'login'});
 });
 
 // -------- Set settings
-router.get('/settings', (req, res) => {
-
+router.get('/settings', ensureAuthenticated, (req, res) => {
     const title = 'Pho Now\'s settings';
     //this is a temporary solution, should go in a controller or helper
     //TODO obtain information from database/model
@@ -72,32 +66,43 @@ router.get('/settings', (req, res) => {
     res.render('./admin/settings', { layout: 'main-admin', title: title, settings: settingsObj });
 });
 
-// -------- Add category
-router.get('/addcategories', (req, res) => {
-    res.render('./admin/add-categ', { layout: 'main-admin' });
+// -------- Menu Categories route
+router.get('/categories', ensureAuthenticated, (req, res) => {
+    const title = 'Pho Now\'s menu categories';
+    //this is a temporary solution, should go in a controller or helper
+    //TODO obtain information from database/model
+    let settingsObj = {
+        "category": {
+            "list": [
+                {
+                    "id": 1,
+                    "category_name": "Noodles",
+                    "category_description": "Glutten free options available",
+                    "isActive": true,
+                    "createdAt": '01/01/2018 13:00:12PM',
+                    "updatedAt": '01/01/2018 13:00:12PM',
+                },
+                {
+                    "id": 2,
+                    "category_name": "Rice",
+                    "category_description": "You can add shrimp on any rice",
+                    "isActive": false,
+                    "createdAt": '01/01/2018 13:00:12PM',
+                    "updatedAt": '01/01/2018 13:00:12PM',
+                }
+            ]
+        }
+    };
+
+    res.render('./admin/categories', { layout: 'main-admin', title: title, settings: settingsObj });
 });
 
-// -------- Add item
-router.get('/additem', (req, res) => {
-    let categories = [{
-        id: 1,
-        category_name: "Kids"
-    },
-    {
-        id: 2,
-        category_name: "Drinks"
-    }];
+// -------- Menu Items route
+router.get('/menuitems', ensureAuthenticated, (req, res) => {
 
-    let menuTypes = [{
-        id: 1,
-        menu_type_name: "Breakfast"
-    },
-    {
-        id: 2,
-        menu_type_name: "Lunch"
-    }];
 
-    /*const title = 'Pho Now\'s menu items';
+
+    const title = 'Pho Now\'s menu items';
     //this is a temporary solution, should go in a controller or helper
     //TODO obtain information from database/model
     let settingsObj = {
@@ -131,22 +136,36 @@ router.get('/additem', (req, res) => {
                 { "id": 2, "name": "Rice" }
             ]
         }
-    };*/
+    };
 
 
-    res.render('./admin/add-item', { layout: 'main-admin', categories: categories ,menuTypes:menuTypes });
+    var menuTypes = {};
+    db.menu_type.findAll({
+    }).then(function (menuTypes) {
+        menuTypes = menuTypes;
+        db.menu_category.findAll({
+        }).then(function (categories) {
+            res.render('./admin/menuitems', { layout: 'main-admin', title: title, settings: settingsObj, categories: categories, menuTypes: menuTypes });
+        });
+    });
 
+});
+
+router.post('/menuitems', ensureAuthenticated, (req, res) => {
+    console.log(req.body);
+    db.menu_items.create(req.body).then(function (data) {
+        res.json(data);
+    });
 });
 
 // -------- fail route
 router.get('/403', (req, res) => {
-    res.render('./admin/403', { layout: 'main-admin' });
-    //res.render('./admin/403');
+    res.render('./admin/403');
 });
 
 // -------- Authorized route - dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-    res.render('./admin/index', { layout: 'main-admin' });
+    res.render('./admin/settings', { layout: 'main-admin' });
 });
 
 
