3cd7dcb6c27d9a6be5b9f853ea333ff38e9ff19c
The Mai <Mai.The.09@gmail.com>
2018-05-18 01:13:41 -0500

    closes #130 add google maps api to /about page

--- public/javascript/main/about.js
+++ public/javascript/main/about.js
@@ -1,1 +1,20 @@
 console.log('LOADED: admin/about.js');
+$(document).ready(function () {
+    initMap();
+});
+
+// Function to initilize google maps API thing
+function initMap() {
+    var myLatLng = { lat: 29.845319, lng: -95.391193 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Pho Now!'
+    });
+}
