f7dad86abc60988a992c8d9c77c76f80aa2699e1
The Mai <Mai.The.09@gmail.com>
2018-05-20 10:53:35 -0500

    fixed landing

--- public/javascript/main/index.js
+++ public/javascript/main/index.js
@@ -1,7 +1,22 @@
 console.log('LOADED: javascript/index.js');
-
 // Sets rellax
 var rellax = new Rellax('.rellax', {
-    
+
 });
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1200, 'easeInOutSine', function () {
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+})
+
+
 
